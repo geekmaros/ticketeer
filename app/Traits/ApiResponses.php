8d9f70cd8d2ef2bacf0fd47881b26c0eeb2f6a63
Mustapha Abdul-Rasaq <razicruz@gmail.com>
2024-09-15 06:33:50 +0100

    feat: add ApiResponses.php file to handle responses

--- app/Traits/ApiResponses.php
+++ app/Traits/ApiResponses.php
@@ -1,1 +1,22 @@
 <?php
+
+namespace App\Traits;
+
+
+use Illuminate\Http\JsonResponse;
+
+trait ApiResponses {
+
+    protected function ok($message): JsonResponse
+    {
+        return $this->success($message, 200);
+    }
+    protected function success($message, $statusCode = 200,): JsonResponse
+    {
+        return response()->json([
+            'message' => $message,
+            'status' => $statusCode,
+
+        ], $statusCode);
+    }
+}
