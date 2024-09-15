643f55e3a4431d0f61049d43149ce852d391aab7
Mustapha Abdul-Rasaq <razicruz@gmail.com>
2024-09-15 08:36:24 +0100

    feat: add Ticket api route to endpoint

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Ticket;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
@@ -7,6 +8,14 @@ use App\Http\Controllers\AuthController;
 
 Route::post('/login', AuthController::class . '@login');
 Route::post('/register', AuthController::class . '@register');
+
+Route::get('/tickets', function () {
+    $tickets = Ticket::all();
+    return response()->json([
+        'message' => $tickets,
+        'status' => 200,
+    ], 200);
+});
 
 Route::get('/user', function (Request $request) {
     return $request->user();
