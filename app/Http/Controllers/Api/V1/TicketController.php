40fa006c5f692f3044b7df9d77699ff5cf40461e
Mustapha Abdul-Rasaq <razicruz@gmail.com>
2024-09-16 15:03:09 +0100

    feat: pass ticket resource to ticket controller fo make our data a collection

--- app/Http/Controllers/Api/V1/TicketController.php
+++ app/Http/Controllers/Api/V1/TicketController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api\V1;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\V1\StoreTicketRequest;
 use App\Http\Requests\Api\V1\UpdateTicketRequest;
+use App\Http\Resources\V1\TicketResource;
 use App\Models\Ticket;
 use App\Traits\ApiResponses;
 
@@ -16,7 +17,7 @@ class TicketController extends Controller
      */
     public function index()
     {
-        return $this->ok('Tickets retrieved successfully', Ticket::all());
+        return TicketResource::collection(Ticket::paginate());
     }
 
     /**
@@ -32,7 +33,7 @@ class TicketController extends Controller
      */
     public function show(Ticket $ticket)
     {
-        //
+        return new TicketResource($ticket);
     }
 
     /**
