d37dd50781b2beb7360435644674196eb4c6c351
Mustapha Abdul-Rasaq <razicruz@gmail.com>
2024-09-27 01:08:21 +0100

    feat: update project files

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -2,6 +2,9 @@
 
 namespace App\Models;
 
+use App\Http\Filters\V1\QueryFilter;
+use App\Http\Filters\V1\TicketFilter;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -9,8 +12,14 @@ class Ticket extends Model
 {
     use HasFactory;
 
-    public function user()
+    public function author()
     {
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function scopeFilter(Builder $builder, QueryFilter $filters)
+    {
+        return $filters->apply($builder);
+
     }
 }
