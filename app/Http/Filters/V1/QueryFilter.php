29033edd993058987da329a99c80db30fc1b971a
Mustapha Abdul-Rasaq <razicruz@gmail.com>
2024-09-27 02:44:17 +0100

    feat: add filter and sort functionality

--- app/Http/Filters/V1/QueryFilter.php
+++ app/Http/Filters/V1/QueryFilter.php
@@ -9,21 +9,11 @@ abstract class QueryFilter
 {
     protected $builder;
     protected $request;
+    protected $sortable = [];
 
     public function __construct(Request $request)
     {
         $this->request = $request;
-    }
-
-    protected function filter($arr){
-
-        foreach ($arr as $key => $value) {
-            if (method_exists($this, $key)) {
-                $this->$key($value);
-            }
-        }
-
-        return $this->builder;
     }
 
     public function apply(Builder $builder): Builder
@@ -39,4 +29,45 @@ abstract class QueryFilter
 
         return $builder;
     }
+
+    protected function filter($arr)
+    {
+
+        foreach ($arr as $key => $value) {
+            if (method_exists($this, $key)) {
+                $this->$key($value);
+            }
+        }
+
+        return $this->builder;
+    }
+
+    protected function sort($value)
+    {
+        $sortAttrs = explode(',', $value);
+
+        foreach ($sortAttrs as $sortAttr) {
+            $direction = 'asc';
+
+            //we can also use strpos
+            if (str_starts_with($sortAttr, '-')) {
+                $direction = 'desc';
+
+                $sortAttr = ltrim($sortAttr, '-');
+
+            }
+
+            if(!in_array($sortAttr, $this->sortable) && !array_key_exists($sortAttr, $this->sortable)) continue;
+
+            $columnName = $this->sortable[$sortAttr] ?? null;
+
+            if($columnName === null){
+                $columnName = $sortAttr;
+            }
+
+            $this->builder->orderBy($columnName, $direction);
+        }
+    }
+
+
 }
